9d1df0ab0f059f22446d8f0ac7a0e4e4cad67440
elha <elias.hahn@mail.de>
2024-07-16 11:19:33 +0200

    fix: switch wrong checked, some refactoring

--- js/cookieHandler.js
+++ js/cookieHandler.js
@@ -1,23 +1,4 @@
-const MODE = "mode";
-const DARK = "dark";
-const LIGHT = "light";
-
-const toggleMode = () => {
-    const elements = getModeElements();
-    console.log("elements", elements);
-    elements.forEach((ele) => {
-        ele.classList.toggle(LIGHT);
-        ele.classList.toggle(DARK);
-    });
-    setCookie(
-        "mode",
-        elements[0].classList.contains(DARK) ? DARK : LIGHT,
-        365
-    );
-}
-
 const setCookie = (cookieName, cookieValue, expirationDays) => {
-    console.log("setCookie", cookieName, cookieValue);
     document.cookie = cookieName + "=" + cookieValue +
         ";expires=" + calcExpirationCookie(expirationDays) + ";path=/";
 }
@@ -45,20 +26,3 @@ const calcExpirationCookie = (expirationDays) => {
     currentDate.setTime(currentDate.getTime() + (expirationDays * 24 * 60 * 60 * 1000));
     return currentDate.toUTCString();
 }
-
-const getModeElements = () => {
-    return document.querySelectorAll("." + MODE);
-}
-
-const onContentLoaded = () => {
-    const elements = getModeElements();
-    elements.forEach((ele) => {
-        const mode = getCookie("mode");
-        ele.classList.add(mode !== "" ? mode : DARK);
-    })
-    document.getElementById("switch__input").checked = elements[0].classList.contains(DARK);
-}
-
-addEventListener("DOMContentLoaded", () => {
-    onContentLoaded();
-});
---
+++ js/main.js
@@ -0,0 +1,39 @@
+const MODE = "mode";
+const DARK = "dark";
+const LIGHT = "light";
+
+const toggleMode = () => {
+    const elements = getModeElements();
+    elements.forEach((ele) => {
+        ele.classList.toggle(LIGHT);
+        ele.classList.toggle(DARK);
+    });
+    setCookie(
+        "mode",
+        elements[0].classList.contains(DARK) ? DARK : LIGHT,
+        365
+    );
+}
+
+const getModeElements = () => {
+    return document.querySelectorAll("." + MODE);
+}
+
+const onContentLoaded = () => {
+    const elements = getModeElements();
+    const mode = getCookie("mode");
+    elements.forEach((ele) => {
+        if (mode === "") {
+            ele.classList.add(DARK);
+        } else {
+            ele.classList.remove(DARK);
+            ele.classList.add(mode);
+        }
+    })
+    console.log(elements[0]);
+    document.getElementById("switch__input").checked = elements[0].classList.contains(DARK);
+}
+
+addEventListener("DOMContentLoaded", () => {
+    onContentLoaded();
+});
